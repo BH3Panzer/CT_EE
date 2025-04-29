88a01cf3ca9a6aea94d769da3f0e2959bde2616d
BH3Panzer <clementcattin74@proton.me>
2025-04-29 20:59:43 +0200

    changed dev generation

--- src/map.rs
+++ src/map.rs
@@ -23,11 +23,17 @@ impl Map {
     }
 
     pub fn dev_gen(&mut self, rl: &mut RaylibHandle, thread: &RaylibThread) {
-        let tree_model: Model = rl.load_model(thread, "src/assets/models/arbre.gltf").unwrap();
         let rock_spot_model: Model = rl.load_model(thread, "src/assets/models/rockSpot.gltf").unwrap();
-        self.add_ressource(Ressource::new(tree_model, Vector3::new(0.0, 0.0, 0.0), Vector3::new(0.0, 0.0, 0.0), 2.0, "tree".to_string(), 100));
+        
         self.add_ressource(Ressource::new(rock_spot_model, Vector3::new(-2.0, 0.0, 5.0), Vector3::new(0.0, 0.0, 0.0), 1.0, "rock".to_string(), 100));
-        for i in 0..5000 {
+        
+        for i in 0..5 {
+            let food_spot_model: Model = rl.load_model(thread, "src/assets/models/foodSpot.gltf").unwrap();
+            self.add_ressource(Ressource::new(food_spot_model, Vector3::new(rand::rng().gen_range(-50.0..50.0), 0.0, rand::rng().gen_range(-50.0..50.0)), Vector3::new(0.0, 0.0, 0.0), 1.0, "food".to_string(), 100));
+        }
+        
+        
+        for i in 0..500 {
             let tree_model: Model = rl.load_model(thread, "src/assets/models/arbre.gltf").unwrap();
             self.add_ressource(Ressource::new(tree_model, Vector3::new(rand::rng().gen_range(-50.0..50.0), 0.0, rand::rng().gen_range(-50.0..50.0)), Vector3::new(0.0, 0.0, 0.0), 2.0, "tree".to_string(), 100));
         }
